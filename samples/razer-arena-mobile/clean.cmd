34b198bf15d5c68a4363cc52b196f554293d31f3
Tim Graupmann <tgraupmann@gmail.com>
2017-03-01 11:30:03 -0800

    Added package argument

---
+++ samples/razer-arena-mobile/clean.cmd
@@ -0,0 +1,4 @@
+CALL rm -r -f arena_client
+CALL rm -r -f arena_control
+CALL rm -r -f arena_game_server
+CALL rm -r -f arena_polling
--- samples/razer-arena-mobile/generate_java_clients.cmd
+++ samples/razer-arena-mobile/generate_java_clients.cmd
@@ -1,5 +1,7 @@
 @ECHO OFF
 
+CALL clean.cmd
+
 ECHO Generating JAVA Clients...
 
 SET OUT_DIR=%CD%
@@ -7,18 +9,22 @@ SET SW_DIR=%CD%\..\..
 
 SET OUT_NAME=arena_client
 SET OUT_URL=https://client.arena.razerzone.com/swagger/docs/v1
+SET PACKAGE=com.razerzone.sdk.arena.client
 call:genFunc
 
 SET OUT_NAME=arena_control
 SET OUT_URL=https://control.arena.razerzone.com/swagger/docs/v1
+SET PACKAGE=com.razerzone.sdk.arena.control
 call:genFunc
 
 SET OUT_NAME=arena_game_server
 SET OUT_URL=https://gameserver.arena.razerzone.com/swagger/docs/v1
+SET PACKAGE=com.razerzone.sdk.arena.gameserver
 call:genFunc
 
 SET OUT_NAME=arena_polling
 SET OUT_URL=https://polling.arena.razerzone.com/swagger/docs/v1
+SET PACKAGE=com.razerzone.sdk.arena.polling
 call:genFunc
 
 ECHO JAVA Clients Generated [DONE]
@@ -29,9 +35,8 @@ EXIT
 :genFunc
 ECHO Generating (%OUT_NAME%)...
 CD "%OUT_DIR%"
-CALL rm -r -f "%OUT_NAME%"
 REM PAUSE
 cd "%SW_DIR%"
-CALL java -jar modules\swagger-codegen-cli\target\swagger-codegen-cli.jar generate -i "%OUT_URL%" -l java -o "%OUT_DIR%\%OUT_NAME%"
+CALL java -jar modules\swagger-codegen-cli\target\swagger-codegen-cli.jar generate -i "%OUT_URL%" -l java -o "%OUT_DIR%\%OUT_NAME%" --api-package %PACKAGE%
 ECHO Generated (%OUT_NAME%) [DONE]
 GOTO:eof
