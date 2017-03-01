6d263b46671933864ab5a48acdba1c45f8fc2889
Tim Graupmann <tgraupmann@gmail.com>
2017-03-01 12:50:58 -0800

    Added model namespace. Added one more package level so that libraries can coexist in the same project.

--- samples/razer-arena-mobile/generate_java_clients.cmd
+++ samples/razer-arena-mobile/generate_java_clients.cmd
@@ -9,22 +9,22 @@ SET SW_DIR=%CD%\..\..
 
 SET OUT_NAME=arena_client
 SET OUT_URL=https://client.arena.razerzone.com/swagger/docs/v1
-SET PACKAGE=com.razerzone.sdk.arena.client
+SET PACKAGE=com.razerzone.sdk.arena.client.swagger
 call:genFunc
 
 SET OUT_NAME=arena_control
 SET OUT_URL=https://control.arena.razerzone.com/swagger/docs/v1
-SET PACKAGE=com.razerzone.sdk.arena.control
+SET PACKAGE=com.razerzone.sdk.arena.control.swagger
 call:genFunc
 
 SET OUT_NAME=arena_game_server
 SET OUT_URL=https://gameserver.arena.razerzone.com/swagger/docs/v1
-SET PACKAGE=com.razerzone.sdk.arena.gameserver
+SET PACKAGE=com.razerzone.sdk.arena.gameserver.swagger
 call:genFunc
 
 SET OUT_NAME=arena_polling
 SET OUT_URL=https://polling.arena.razerzone.com/swagger/docs/v1
-SET PACKAGE=com.razerzone.sdk.arena.polling
+SET PACKAGE=com.razerzone.sdk.arena.polling.swagger
 call:genFunc
 
 ECHO JAVA Clients Generated [DONE]
@@ -37,6 +37,6 @@ ECHO Generating (%OUT_NAME%)...
 CD "%OUT_DIR%"
 REM PAUSE
 cd "%SW_DIR%"
-CALL java -jar modules\swagger-codegen-cli\target\swagger-codegen-cli.jar generate -i "%OUT_URL%" -l java -o "%OUT_DIR%\%OUT_NAME%" --api-package %PACKAGE%
+CALL java -jar modules\swagger-codegen-cli\target\swagger-codegen-cli.jar generate -i "%OUT_URL%" -l java -o "%OUT_DIR%\%OUT_NAME%" --api-package %PACKAGE% --model-package %PACKAGE%.model
 ECHO Generated (%OUT_NAME%) [DONE]
 GOTO:eof
