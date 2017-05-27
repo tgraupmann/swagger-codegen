12b95b2429417f1613fa9a620a8b3abf00dea404
Tim Graupmann <tgraupmann@gmail.com>
2017-05-26 19:11:38 -0700

    Added debug REM to get help commands

--- samples/chromasdk/generate_csharp_client.cmd
+++ samples/chromasdk/generate_csharp_client.cmd
@@ -9,17 +9,19 @@ ECHO Generating CSHARP .Net 2 Client...
 SET OUT_DIR=%CD%
 SET SW_DIR=%CD%\..\..
 
+SET OUT_GEN=csharp
+SET OUT_NAME=chromasdk_%OUT_GEN%
+SET OUT_URL=https://raw.githubusercontent.com/tgraupmann/ChromaSwaggerDefinition/master/api/swagger/swagger.yaml
+SET PACKAGE=chromasdk
+call:genFunc
+
+
 SET OUT_GEN=CsharpDotNet2
 SET OUT_NAME=chromasdk_%OUT_GEN%
 SET OUT_URL=https://raw.githubusercontent.com/tgraupmann/ChromaSwaggerDefinition/master/api/swagger/swagger.yaml
 SET PACKAGE=chromasdk
 call:genFunc
 
-SET OUT_GEN=csharp
-SET OUT_NAME=chromasdk_%OUT_GEN%
-SET OUT_URL=https://raw.githubusercontent.com/tgraupmann/ChromaSwaggerDefinition/master/api/swagger/swagger.yaml
-SET PACKAGE=chromasdk
-call:genFunc
 
 ECHO Clients Generated [DONE]
 
@@ -31,6 +33,9 @@ ECHO Generating (%OUT_NAME%)...
 CD "%OUT_DIR%"
 REM PAUSE
 cd "%SW_DIR%"
-CALL java -jar modules\swagger-codegen-cli\target\swagger-codegen-cli.jar generate -i "%OUT_URL%" -l %OUT_GEN% -o "%OUT_DIR%\%OUT_NAME%" --api-package %PACKAGE% --model-package %PACKAGE%.model -DhideGenerationTimestamp=true
+REM CALL java -jar modules\swagger-codegen-cli\target\swagger-codegen-cli.jar config-help -l %OUT_GEN%
+REM PAUSE
+REM CALL java -jar modules\swagger-codegen-cli\target\swagger-codegen-cli.jar generate -i "%OUT_URL%" -l %OUT_GEN% -o "%OUT_DIR%\%OUT_NAME%" --api-package %PACKAGE% --model-package %PACKAGE%.model -DtargetFramework=v3.5
+CALL java -jar modules\swagger-codegen-cli\target\swagger-codegen-cli.jar generate -i "%OUT_URL%" -l %OUT_GEN% -o "%OUT_DIR%\%OUT_NAME%" --api-package %PACKAGE% --model-package %PACKAGE%.model
 ECHO Generated (%OUT_NAME%) [DONE]
 GOTO:eof
