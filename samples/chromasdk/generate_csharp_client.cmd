4c7da761515eaa5ba1937a4d99104838c8629ec0
Tim Graupmann <tgraupmann@gmail.com>
2017-05-30 13:26:31 -0700

    Use custom definition to workaround parameters with different types

--- samples/chromasdk/generate_csharp_client.cmd
+++ samples/chromasdk/generate_csharp_client.cmd
@@ -9,18 +9,36 @@ ECHO Generating CSHARP .Net 2 Client...
 SET OUT_DIR=%CD%
 SET SW_DIR=%CD%\..\..
 
+
+REM Generate API
+
 SET OUT_GEN=csharp
 SET OUT_NAME=chromasdk_%OUT_GEN%
 SET OUT_URL=https://raw.githubusercontent.com/tgraupmann/ChromaSwaggerDefinition/master/api/swagger/swagger.yaml
-SET PACKAGE=chromasdk
+SET PACKAGE=ChromaPackage
+SET NAMESPACE=ChromaSDK
+SET MODEL=%PACKAGE%.Model
 call:genFunc
 
 
-SET OUT_GEN=CsharpDotNet2
-SET OUT_NAME=chromasdk_%OUT_GEN%
-SET OUT_URL=https://raw.githubusercontent.com/tgraupmann/ChromaSwaggerDefinition/master/api/swagger/swagger.yaml
-SET PACKAGE=chromasdk
+REM Generate Custom API to workaround fields with multiple types
+
+SET OUT_GEN=csharp
+SET OUT_NAME=custom_chromasdk_%OUT_GEN%
+SET OUT_URL=https://raw.githubusercontent.com/tgraupmann/ChromaSwaggerDefinition/master/api/swagger/swagger_custom.yaml
+SET PACKAGE=CustomChromaPackage
+SET NAMESPACE=CustomChromaSDK
+SET MODEL=%PACKAGE%.Model
 call:genFunc
+
+
+REM SET OUT_GEN=CsharpDotNet2
+REM SET OUT_NAME=chromasdk_%OUT_GEN%
+REM SET OUT_URL=https://raw.githubusercontent.com/tgraupmann/ChromaSwaggerDefinition/master/api/swagger/swagger.yaml
+REM SET PACKAGE=ChromaPackage
+REM SET NAMESPACE=ChromaSDK
+REM SET MODEL=%PACKAGE%.Model
+REM call:genFunc
 
 
 ECHO Clients Generated [DONE]
@@ -33,9 +51,9 @@ ECHO Generating (%OUT_NAME%)...
 CD "%OUT_DIR%"
 REM PAUSE
 cd "%SW_DIR%"
+REM CALL java -jar modules\swagger-codegen-cli\target\swagger-codegen-cli.jar help
 REM CALL java -jar modules\swagger-codegen-cli\target\swagger-codegen-cli.jar config-help -l %OUT_GEN%
-REM PAUSE
-REM CALL java -jar modules\swagger-codegen-cli\target\swagger-codegen-cli.jar generate -i "%OUT_URL%" -l %OUT_GEN% -o "%OUT_DIR%\%OUT_NAME%" --api-package %PACKAGE% --model-package %PACKAGE%.model -DtargetFramework=v3.5
-CALL java -jar modules\swagger-codegen-cli\target\swagger-codegen-cli.jar generate -i "%OUT_URL%" -l %OUT_GEN% -o "%OUT_DIR%\%OUT_NAME%" --api-package %PACKAGE% --model-package %PACKAGE%.model
+REM CALL java -jar modules\swagger-codegen-cli\target\swagger-codegen-cli.jar generate -i "%OUT_URL%" -l %OUT_GEN% -o "%OUT_DIR%\%OUT_NAME%" --api-package %PACKAGE% --model-package %MODEL% -DtargetFramework=v3.5
+CALL java -jar modules\swagger-codegen-cli\target\swagger-codegen-cli.jar generate -i "%OUT_URL%" -l %OUT_GEN% -o "%OUT_DIR%\%OUT_NAME%" --api-package %PACKAGE% --model-package %MODEL% -DpackageName=%NAMESPACE%
 ECHO Generated (%OUT_NAME%) [DONE]
 GOTO:eof
